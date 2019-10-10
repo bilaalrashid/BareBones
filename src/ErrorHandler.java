65f556f191f29d35fdc39f3be27df87e08f8bf66
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-10-10 13:08:07 +0100

    Added script path argument to interpreter

---
+++ src/ErrorHandler.java
@@ -0,0 +1,13 @@
+/**
+ * Handles errors found by the interpreter
+ */
+class ErrorHandler {
+
+    /**
+     * Crashes the execution of the BareBones script
+     */
+    static void crash() {
+        System.exit(0);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+/**
+ * The BareBones interpreter
+ */
 public class Main {
 
+    /**
+     * Interprets the specified BareBones file
+     * @param args The command line arguments
+     */
     public static void main(String[] args) {
+        if (args != null && args.length != 0) {
+            String path = args[0];
+        }
 
+        ErrorHandler.crash();
     }
 
 }
